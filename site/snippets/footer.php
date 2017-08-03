0a1821cca1b25bf172ca4a5c12501de4adc8ae53
Peter Friend <pete_applewood12391@hotmail.co.uk>
2017-08-03 21:43:32 +0200

    Load unminified scripts if env is development

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -14,5 +14,26 @@
     </div>
   </footer>
 
+  <?php 
+
+    switch (c::get('env')) {
+
+      case 'production':
+        echo js(array(
+          'assets/production/all.min.js'
+        ));
+      break;
+
+      case 'development':
+        echo js(array(
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/bootstrap/dist/js/bootstrap.js',
+          'assets/js/main.js'
+        ));
+      break;
+
+    }
+
+  ?>
 </body>
 </html>
